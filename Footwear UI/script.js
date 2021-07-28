1c618c2dcd3d64274d337b0ff6c32389e8ea8b32
OmarDefaoui <omardefaoui@gmail.com>
2021-07-28 23:12:41 +0100

    Add Footwear UI website

--- Footwear UI/script.js
+++ Footwear UI/script.js
@@ -3,8 +3,16 @@ const image = document.querySelector('.slide');
 const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 
+const productName = document.getElementById('product_name');
+const indicators = document.querySelectorAll('.indicator');
+
+const PRODUCTS_TITLE = ['Performance', 'Design', 'Comodity', 'Lightness'];
+const BACKGROUNDS_COLOR = ['#cb021c', '#E0E1E3', '#F6F7F9', '#3F4346'];
+const TEXT_COLOR = ['#ffffff', '#000000', '#000000', '#ffffff'];
+
 const SLIDES_COUNT = slides.length;
 let currentSlide = 0;
+indicators[0].style.background = `white`;
 
 right.addEventListener('click', () => {
     console.log('right button clicked');
@@ -24,7 +32,17 @@ left.addEventListener('click', () => {
     updateSlider();
 });
 
-function updateSlider(){
-    image.style.transform = `translateX(${currentSlide*slides[0].offsetWidth}px)`;
-    console.log(currentSlide*slides[0].offsetWidth);
+function updateSlider() {
+    // Change displayed picture
+    image.style.transform = `translateX(${-currentSlide * slides[0].offsetWidth}px)`;
+
+    document.body.style.background = BACKGROUNDS_COLOR[currentSlide];
+    productName.innerHTML = PRODUCTS_TITLE[currentSlide];
+    productName.style.color = TEXT_COLOR[currentSlide];
+
+    // Change selected indicator color
+    indicators.forEach((indicator) => {
+        indicator.style.background = `rgba(255, 255, 255, 0.6)`;
+    });
+    indicators[currentSlide].style.background = `white`;
 }
--- README.md
+++ README.md
@@ -22,3 +22,12 @@ An Automotive Car UI with responsive design.
 <div style="display: inline-block; width: 100%;">
     <img src="screenshots/Automotive Car/auto_ui_s1.png" width="100%"></img>
 </div>
+
+# Footwear UI
+
+An Footwear UI with responsive design.
+
+<div style="display: inline-block; width: 100%;">
+    <img src="screenshots/Footwear/1.png" width="100%"></img>
+    <img src="screenshots/Footwear/2.png" width="100%"></img>
+</div>
